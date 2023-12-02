8c8009a60cd1878926d77e090b73b053c245f642
siraj <112253370+SirajBagwan@users.noreply.github.com>
2023-12-03 01:05:32 +0530

    Update ec2.tf

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,1 +1,38 @@
+# Creaing secodary network interface (ENI) for instance
+resource "aws_network_interface" "Eni" {
+  subnet_id   = var.subnetId
+  private_ips = [var.secondary_ip]
+}
 
+# Attaching the ENI with EC2 as Secondary IP
+resource "aws_network_interface_attachment" "test" {
+  instance_id          = aws_instance.web.id
+  network_interface_id = aws_network_interface.Eni.id
+  device_index         = 1
+  depends_on           = [aws_instance.web, aws_network_interface.Eni]
+}
+
+# Launching an EC2 instances
+resource "aws_instance" "web" {
+  count                       = var.Count
+  ami                         = var.aws_ami_id
+  instance_type               = var.Instance_Type
+  associate_public_ip_address = true
+  availability_zone           = var.az
+  subnet_id                   = var.subnetId
+  vpc_security_group_ids      = var.SGroup
+  key_name                    = var.access_key
+  user_data                   = <<-EOF
+                                  #!/bin/bash
+                                  apt-get update -y
+                                  apt-get apache2 -y
+                                  echo "Hello From $(hostname -f)" > /var/www/html/index.html
+                                  EOF
+  root_block_device {
+    volume_size           = var.RTD_Size
+    delete_on_termination = false
+  }
+  tags = {
+    Name = "Web_Server"
+  }
+}
