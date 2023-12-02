47f71ba27f2521eacd3201c95a65e487cd7e6e93
siraj <112253370+SirajBagwan@users.noreply.github.com>
2023-12-03 00:39:11 +0530

    Update variables.tf

--- EC2/variables.tf
+++ EC2/variables.tf
@@ -1,1 +1,31 @@
+variable "aws_ami_id" {
+  default = "ami-0a7cf821b91bcccbc"
+}
 
+variable "access_key" {
+  default = "newkey"
+}
+
+variable "secondary_ip" {
+  default = "10.0.0.7"
+}
+
+variable "Instance_Type" {
+  default = "t2.micro"
+}
+
+variable "Count" {
+  default = 1
+}
+
+variable "subnetId"{
+  default = "Update id of subnet from UI"
+}
+
+variable "SGroup"{
+  default = "Update id of SG from UI"
+}
+
+variable "RTD_Size"{
+  default = 50
+}
